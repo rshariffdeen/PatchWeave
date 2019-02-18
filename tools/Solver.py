8a5bfbe7b2de7738982b81a0bd244672b1144579
rshariffdeen <rshariffdeen@gmail.com>
2019-02-18 17:04:11 +0800

    adding new method estimate div point

--- tools/Solver.py
+++ tools/Solver.py
@@ -7,62 +7,25 @@ from phases import Trace, Concolic
 import Logger
 import Mapper
 import Emitter
+import Extractor
+import Identifier
 
 
-def estimate_divergent_point(byte_list):
-    Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
-    Emitter.normal("\tfinding similar location in recipient")
-    length = len(Concolic.sym_path_c.keys()) - 1
-    count_common = len(byte_list)
-    candidate_list = list()
-    estimated_loc = ""
-    for n in range(length, 0, -1):
-        key = Concolic.sym_path_c.keys()[n]
-        sym_path = Concolic.sym_path_c[key]
-        bytes_temp = Mapper.get_input_bytes_used(sym_path)
-        count = len(list(set(byte_list).intersection(bytes_temp)))
-        if count == count_common:
-            candidate_list.append(key)
-    length = len(Trace.list_trace_c) - 1
-    grab_nearest = False
-    for n in range(length, 0, -1):
-        path = Trace.list_trace_c[n]
-        path = os.path.abspath(path)
-        if grab_nearest:
-            if ".c" in path:
-                estimated_loc = path
-                break
-        else:
-            if path in candidate_list:
-                if ".h" in path:
-                    grab_nearest = True
-                else:
-                    estimated_loc = path
-                    break
-    return estimated_loc
-
-
-def translate_patch(patch_code, var_map):
-    for var in var_map.keys():
-        if var in patch_code:
-            str(patch_code).replace(var, var_map[var])
-    return patch_code
-
-
-def insert_patch(patch_code, source_path, line_number):
-    Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
-    global modified_source_list
-    content = ""
-    if source_path not in modified_source_list:
-        modified_source_list.append(source_path)
-    if os.path.exists(source_path):
-        with open(source_path, 'r') as source_file:
-            content = source_file.readlines()
-            existing_statement = content[line_number]
-            content[line_number] = patch_code + existing_statement
-
-    with open(source_path, 'w') as source_file:
-        source_file.writelines(content)
+def estimate_divergent_point(path_cond_a, path_cond_b):
+    Emitter.sub_sub_title("estimating divergent point in Pc")
+    Emitter.normal("\textracting input bytes from div point in Pa")
+    bytes_a = Extractor.extract_input_bytes_used(path_cond_a)
+    Emitter.normal("\textracting input bytes from last path condition in Pc")
+    bytes_c = Extractor.extract_input_bytes_used(path_cond_b)
+    # print(bytes_c)
+    byte_list = Extractor.extract_common_bytes(bytes_a, bytes_c)
+    # print(byte_list)
+    Emitter.normal("\testimating divergent point")
+    estimate_loc = Identifier.identify_divergent_point(byte_list,
+                                                       Concolic.sym_path_c,
+                                                       Trace.list_trace_c
+                                                       )
+    return estimate_loc
 
 
 def get_best_insertion_point(insertion_point_list):
