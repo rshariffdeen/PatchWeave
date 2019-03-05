62c94f6c920119776d4d8f8cca038aadd2dde7a5
rshariffdeen <rshariffdeen@gmail.com>
2019-03-05 17:10:32 +0800

    new line char added except for last line

--- tools/Writer.py
+++ tools/Writer.py
@@ -19,7 +19,7 @@ def write_skip_list(skip_list, output_file):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     content = "0\n"
     for line_number in skip_list:
-        content += str(line_number) +  "\n"
+        content += str(line_number) + "\n"
     with open(output_file, 'w') as map_file:
         map_file.writelines(content)
 
@@ -28,6 +28,8 @@ def write_ast_script(ast_script, output_file):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     content = ""
     with open(output_file, 'w') as script_file:
-	for op in ast_script:
-            content +=  op + "\n"
+        for op in ast_script:
+            content += op
+            if op != ast_script[-1]:
+                content += "\n"
         script_file.writelines(content)
