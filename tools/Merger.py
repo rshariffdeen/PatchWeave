01489fba874a26a2809a6e4427ea6f71a014f9d8
rshariffdeen <rshariffdeen@gmail.com>
2019-03-14 18:08:01 +0800

    add function to merge macro info

--- tools/Merger.py
+++ tools/Merger.py
@@ -3,7 +3,7 @@
 
 
 import sys
-from common.Utilities import execute_command, get_file_extension_list
+from common.Utilities import execute_command, get_file_extension_list, error_exit
 from ast import ASTGenerator
 import Mapper
 import Logger
@@ -21,3 +21,18 @@ def merge_var_info(var_expr, var_value):
         info["expr_list"] = var_expr[var_name]['expr_list']
         var_info[var_name] = info
     return var_info
+
+
+def merge_macro_info(info_a, info_b):
+    Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
+    macro_info = dict()
+    for macro_name in info_a:
+        info = info_a[macro_name]
+        if macro_name in info_b.keys():
+            error_exit("MULTIPLE USAGE OF MACRO")
+        macro_info[macro_name] = info
+
+    for macro_name in info_b:
+        info = info_b[macro_name]
+        macro_info[macro_name] = info
+    return macro_info
