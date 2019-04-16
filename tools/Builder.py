8b805df21269ada9a14d3a8bedaaf3f7e62ec572
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-04-16 22:49:58 +0800

    change klee lib dir

--- tools/Builder.py
+++ tools/Builder.py
@@ -177,7 +177,7 @@ def build_instrumented_code(source_directory):
     CC = "wllvm"
     CXX = "wllvm++"
     CXX_FLAGS = "'-g -O0 -static -DNDEBUG '"
-    C_FLAGS = "'-g -O0 -static  -L/home/ridwan/workspace/klee/build/lib -lkleeRuntest'"
+    C_FLAGS = "'-g -O0 -static  -L/klee/build/lib -lkleeRuntest'"
 
     if os.path.exists(source_directory + "/" + "aclocal.m4"):
         pre_config_command = "cd " + source_directory + ";"
@@ -271,7 +271,7 @@ def build_llvm():
     C_FLAGS = "'-g -O0 -static'"
     config_all()
     CXX_FLAGS = "'-g -O0 -static -DNDEBUG '"
-    C_FLAGS = "'-g -O0 -static  -L/home/ridwan/workspace/klee/build/lib -lkleeRuntest'"
+    C_FLAGS = "'-g -O0 -static  -L/klee/build/lib -lkleeRuntest'"
     build_all()
 
 
