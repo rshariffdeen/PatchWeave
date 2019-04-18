83ff4422fcc9189e8fc9f43511b61d4c4c8a225a
rshariffdeen <rshariffdeen@gmail.com>
2019-04-18 15:51:25 +0800

    pass target path as arg

--- tools/Identifier.py
+++ tools/Identifier.py
@@ -88,7 +88,7 @@ def identify_missing_var(function_node_a, function_node_b, insert_node_b, skip_l
     return missing_var_list
 
 
-def identify_missing_data_types(insert_node_b, var_info, ast_node_b, ast_node_c):
+def identify_missing_data_types(insert_node_b, var_info, target_path, ast_node_b, ast_node_c):
     Logger.trace(__name__ + ":" + sys._getframe().f_code.co_name, locals())
     Emitter.normal("\t\t\tidentifying missing data-types")
     missing_data_type_list = dict()
@@ -96,8 +96,6 @@ def identify_missing_data_types(insert_node_b, var_info, ast_node_b, ast_node_c)
     ref_list = Extractor.extract_reference_node_list(insert_node_b)
     type_def_node_list_b = Extractor.extract_typedef_node_list(ast_node_b)
     type_def_node_list_c = Extractor.extract_typedef_node_list(ast_node_c)
-    source_path = str(ast_node_b['filename'])
-    target_path = str(ast_node_c['filename'])
     for ref_node in ref_list:
         # print(ref_node)
         node_type = str(ref_node['type'])
@@ -112,7 +110,6 @@ def identify_missing_data_types(insert_node_b, var_info, ast_node_b, ast_node_c)
                 if identifier not in type_def_node_list_c:
                     if identifier not in missing_data_type_list.keys():
                         info = dict()
-                        info['source'] = source_path
                         info['target'] = target_path
                         info['ast-node'] = type_def_node_list_b[identifier]
                         missing_data_type_list[identifier] = info
@@ -121,7 +118,6 @@ def identify_missing_data_types(insert_node_b, var_info, ast_node_b, ast_node_c)
         if identifier not in type_def_node_list_c:
             if identifier not in missing_data_type_list.keys():
                 info = dict()
-                info['source'] = source_path
                 info['target'] = target_path
                 info['ast-node'] = type_def_node_list_b[identifier]
                 missing_data_type_list[identifier] = info
