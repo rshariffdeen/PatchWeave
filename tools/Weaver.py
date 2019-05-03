b7bd8d2009c74b9adefc940f878b3ba4b7311955
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-05-03 21:12:23 +0800

    avoid if backporting

--- tools/Weaver.py
+++ tools/Weaver.py
@@ -124,7 +124,7 @@ def weave_definitions(missing_definition_list, modified_source_list):
                     if def_name in macro_def.split(" "):
                         transplant_code += "\n" + macro_def + "\n"
 
-        if transplant_code == "":
+        if transplant_code == "" and not Values.BACKPORT:
             header_file = Finder.find_header_file(def_name, source_file)
             # print(header_file)
             if header_file is not None:
