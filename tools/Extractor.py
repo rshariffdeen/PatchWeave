b2500e29c92497dec18844e8c58848a35845d31e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-05-03 09:02:10 +0800

    add switch and case nodes

--- tools/Extractor.py
+++ tools/Extractor.py
@@ -283,7 +283,9 @@ def extract_var_ref_list(ast_node, start_line, end_line, only_in_range):
                 var_list.append((str(var_name), insert_line, var_type))
         var_list = var_list + extract_var_ref_list(body_node, start_line, end_line, only_in_range)
         return var_list
-    if node_type in ["IfStmt"]:
+    if node_type in ["CaseStmt"]:
+        return var_list
+    if node_type in ["IfStmt", "SwitchStmt"]:
         condition_node = ast_node['children'][0]
         body_node = ast_node['children'][1]
         insert_line = body_node['start line']
