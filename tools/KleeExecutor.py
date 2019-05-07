0aba29fb9c9ebfbd8987233346607fa3ba89a24e
rshariffdeen <rshariffdeen@gmail.com>
2019-05-07 18:04:49 +0800

    reduce time to 80

--- tools/KleeExecutor.py
+++ tools/KleeExecutor.py
@@ -10,10 +10,10 @@ import Logger
 
 SYMBOLIC_CONVERTER = "gen-bout"
 SYMBOLIC_ENGINE = "klee "
-SYMBOLIC_ARGUMENTS_FOR_PATH = "-print-path  -write-smt2s  --libc=uclibc --posix-runtime --external-calls=all --only-replay-seeds --seed-out=$KTEST -max-time=180 -max-memory=2048"
-SYMBOLIC_ARGUMENTS_FOR_EXPR = " --resolve-path --libc=uclibc --posix-runtime --external-calls=all --only-replay-seeds --seed-out=$KTEST  -max-time=180 -max-memory=2048"
-SYMBOLIC_ARGUMENTS_FOR_VALUE = " --resolve-path --libc=uclibc --posix-runtime --external-calls=all  -max-time=180 -max-memory=2048"
-SYMBOLIC_ARGUMENTS_FOR_TRACE = "--posix-runtime --libc=uclibc --print-trace --print-stack -max-time=180 -max-memory=2048 "
+SYMBOLIC_ARGUMENTS_FOR_PATH = "-print-path  -write-smt2s  --libc=uclibc --posix-runtime --external-calls=all --only-replay-seeds --seed-out=$KTEST -max-time=80 -max-memory=2048"
+SYMBOLIC_ARGUMENTS_FOR_EXPR = " --resolve-path --libc=uclibc --posix-runtime --external-calls=all --only-replay-seeds --seed-out=$KTEST  -max-time=80 -max-memory=2048"
+SYMBOLIC_ARGUMENTS_FOR_VALUE = " --resolve-path --libc=uclibc --posix-runtime --external-calls=all  -max-time=80 -max-memory=2048"
+SYMBOLIC_ARGUMENTS_FOR_TRACE = "--posix-runtime --libc=uclibc --print-trace --print-stack -max-time=80 -max-memory=2048 "
 
 
 def generate_path_condition(binary_arguments, binary_path, binary_name, bit_size, poc_path, log_path, klee_flags):
