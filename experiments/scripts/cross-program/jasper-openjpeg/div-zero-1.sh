3396acf471d99939c1176f45d6e32dbc9c501b5e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-08-01 12:39:42 +0800

    prepare 8691 for jasper-openjpeg

--- experiments/scripts/cross-program/jasper-openjpeg/div-zero-1.sh
+++ experiments/scripts/cross-program/jasper-openjpeg/div-zero-1.sh
@@ -1,6 +1,6 @@
-bug_id=openjpeg-divzero-1
+project_name=jasper-openjpeg
+bug_id=div-zero-1
 dir_name=$1/$bug_id
-dir_name_docker=/data/$bug_id
 pa=jasper-1.900.2
 pb=jasper-1.900.3
 pc=openjpeg-1.5.1
@@ -37,9 +37,9 @@ sed -i "s/get_file_format(infile)/$opj_input/g" $opj_file
 git add $opj_file
 git commit -m "fix input format"
 
-docker exec patchweave bash -c "cd $dir_name_docker/$pc;autoreconf -i;./configure"
-docker exec patchweave bash -c "cd $dir_name_docker/$pc; bear make"
-docker exec patchweave python /patchweave/script/format.py $dir_name_docker/$pc
+cd $dir_name/$pc;autoreconf -i;./configure
+cd $dir_name/$pc; bear make
+python /patchweave/script/format.py $dir_name/$pc
 git add *.c
 git commit -m "format style"
 git reset --hard HEAD
