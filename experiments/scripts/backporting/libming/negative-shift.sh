334f38962743d9033a0b8b7e4b003a75b160600b
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-08-02 16:41:39 +0800

    seting up libming backport

--- experiments/scripts/backporting/libming/negative-shift.sh
+++ experiments/scripts/backporting/libming/negative-shift.sh
@@ -1,15 +1,15 @@
-bug_id=CVE-2016-9828
+bug_id=negative-shift
 project_name=libming
 dir_name=$1/backport/$project_name/$bug_id
-dir_name_docker=/data/backport/$project_name/$bug_id
+
 
 project_url=https://github.com/libming/libming.git
 pa=$project_name-0.4.7
 pb=$project_name-0.4.8
 pc=$project_name-0.4.6
 
-pa_commit=459fa79
-pb_commit=80ebea95
+pa_commit=19e7127
+pb_commit=2e5a98a0
 pc_commit=ming-0_4_6
 
 
@@ -27,9 +27,10 @@ git checkout $pb_commit
 cd ../$pc
 git checkout $pc_commit
 
-docker exec patchweave bash -c "cd $dir_name_docker/$pc;autoreconf -i;./configure"
-docker exec patchweave bash -c "cd $dir_name_docker/$pc; bear make"
-docker exec patchweave python /patchweave/script/format.py $dir_name_docker/$pc
+cd $dir_name/$pc;autoreconf -i;./configure
+cd $dir_name/$pc; bear make
+python /patchweave/script/python/format.py $dir_name/$pc
+
 git add *.c
 git commit -m "format style"
 git reset --hard HEAD
