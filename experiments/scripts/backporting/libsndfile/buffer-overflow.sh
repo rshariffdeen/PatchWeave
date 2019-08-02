e65be58965ffda7bb11dd0ec8c4a50a0a7f2d232
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2019-08-02 16:53:37 +0800

    add libsndfile backport

--- experiments/scripts/backporting/libsndfile/buffer-overflow.sh
+++ experiments/scripts/backporting/libsndfile/buffer-overflow.sh
@@ -1,19 +1,18 @@
-bug_id=CVE-2017-8363
+bug_id=buffer-overflow
 project_name=libsndfile
 dir_name=$1/backport/$project_name/$bug_id
-dir_name_docker=/data/backport/$project_name/$bug_id
 
 project_url=https://github.com/erikd/libsndfile.git
 pa=$project_name-1.0.29
 pb=$project_name-1.0.28
-pc=$project_name-1.0.25
+pc=$project_name-1.0.26
 
 pa_commit=5206a9b
 pb_commit=fd0484ab
-pc_commit=1.0.25
+pc_commit=1.0.26
 
 
-mkdir $dir_name
+mkdir -p $dir_name
 cd $dir_name
 git clone $project_url $pa
 cp -rf $pa $pb
@@ -27,9 +26,10 @@ git checkout $pb_commit
 cd ../$pc
 git checkout $pc_commit
 
-docker exec patchweave bash -c "cd $dir_name_docker/$pc;autoreconf -i;./configure"
-docker exec patchweave bash -c "cd $dir_name_docker/$pc; bear make"
-docker exec patchweave python /patchweave/script/format.py $dir_name_docker/$pc
+cd $dir_name/$pc;autoreconf -i;./configure
+cd $dir_name/$pc; bear make
+python /patchweave/script/python/format.py $dir_name/$pc
+
 git add *.c
 git commit -m "format style"
 git reset --hard HEAD
