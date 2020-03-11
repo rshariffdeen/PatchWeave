41bddf302865b97c2b8a68bac909579ae40f1a91
rshariffdeen <rshariffdeen@gmail.com>
2020-03-11 13:25:17 +0800

    update verifying method

--- tools/Comparer.py
+++ tools/Comparer.py
@@ -49,8 +49,10 @@ def compare_test_output(output_c, output_d):
                             # print(runtime_error_count_c, runtime_error_count_d)
                             if runtime_error_count_d < runtime_error_count_c:
                                 return 1
-                            else:
+                            elif runtime_error_count_c == runtime_error_count_d:
                                 return 0
+                            else:
+                                return -1
                         else:
                             return 1
                     else:
@@ -59,7 +61,24 @@ def compare_test_output(output_c, output_d):
                         else:
                             return 0
                 else:
-                    print(program_output_c)
-                    print(program_output_d)
-                    return -1
+                    if Oracle.any_runtime_error(program_output_c):
+                        if Oracle.any_runtime_error(program_output_d):
+                            program_output_c = "\n".join(program_output_c)
+                            program_output_d = "\n".join(program_output_d)
+                            runtime_error_count_c = program_output_c.count("runtime error")
+                            runtime_error_count_d = program_output_d.count("runtime error")
+                            # print(runtime_error_count_c, runtime_error_count_d)
+                            if runtime_error_count_d < runtime_error_count_c:
+                                return 1
+                            elif runtime_error_count_c == runtime_error_count_d:
+                                return 0
+                            else:
+                                return -1
+                        else:
+                            return 1
+                    else:
+                        if Oracle.any_runtime_error(program_output_d):
+                            return -1
+                        else:
+                            return 0
 
