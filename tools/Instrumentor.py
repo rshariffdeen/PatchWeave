6573467916c1b56e64bc3c0f45dd00ad12ae56ab
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-03-17 15:07:28 +0800

    get expression of pointers

--- tools/Instrumentor.py
+++ tools/Instrumentor.py
@@ -31,9 +31,9 @@ def instrument_klee_var_expr(source_path, start_line, end_line, stack_info, skip
         if line_number in skip_lines:
             continue
         if "*" in data_type:
-            print_code = data_type.replace("*","") + " " + variable + "-temp = *" + variable + "\n"
+            print_code = data_type.replace("*","") + " " + variable + "-temp = *" + variable + ";\n"
             print_code += "klee_print_expr(\"[var-expr] " + variable + "\", " + variable + "-temp);\n"
-            type_print_code = "klee_print_stmt(\"[var-type]: " + variable + ":" + data_type + "\");\n"
+            type_print_code = "klee_print_stmt(\"[var-type]: *" + variable + ":" + data_type + "\");\n"
         else:
             print_code = "klee_print_expr(\"[var-expr] " + variable + "\", " + variable + ");\n"
             type_print_code = "klee_print_stmt(\"[var-type]: " + variable + ":" + data_type + "\");\n"
