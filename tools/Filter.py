37eb2fc53354d51b24f51ed57ac6536b63f6a913
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-03-19 11:47:41 +0800

    slice replace ast if in skiplines

--- tools/Filter.py
+++ tools/Filter.py
@@ -192,6 +192,17 @@ def filter_ast_script_by_skip_line(ast_script, ast_node_a, ast_node_b, skip_line
                         filtered_ast_script.append(script_line)
             else:
                 filtered_ast_script.append(script_line)
+        elif "Replace" in script_line:
+            node_id_b = int(((script_line.split(" with ")[0]).split("(")[1]).split(")")[0])
+            node_b = Finder.search_ast_node_by_id(ast_node_b, node_id_b)
+            node_type_b = node_b['type']
+            node_line_start = int(node_b['start line'])
+            node_line_end = int(node_b['end line']) + 1
+            target_node_type = str((script_line.split(" into ")[1]).split("(")[0])
+            # print(node_line_start)
+            if node_line_start in skip_lines:
+                continue
+
         else:
             filtered_ast_script.append(script_line)
     return filtered_ast_script
