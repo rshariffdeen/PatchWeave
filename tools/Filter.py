4eea123bfe20ece5d8f334a1010209e88a5715b9
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-03-19 20:54:59 +0800

    get the id of b instead of a

--- tools/Filter.py
+++ tools/Filter.py
@@ -193,7 +193,7 @@ def filter_ast_script_by_skip_line(ast_script, ast_node_a, ast_node_b, skip_line
             else:
                 filtered_ast_script.append(script_line)
         elif "Replace" in script_line:
-            node_id_b = int(((script_line.split(" with ")[0]).split("(")[1]).split(")")[0])
+            node_id_b = int(((script_line.split(" with ")[1]).split("(")[1]).split(")")[0])
             node_b = Finder.search_ast_node_by_id(ast_node_b, node_id_b)
             node_type_b = node_b['type']
             node_line_start = int(node_b['start line'])
